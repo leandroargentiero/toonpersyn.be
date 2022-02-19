0f760ec71c04cc04c57e4bbeb78e98f1a838e7d6
Leandro Argentiero <argentiero.leo@gmail.com>
2022-02-19 16:01:37 +0100

    Added tailwindcss

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500">Hello World!</h1>
+  );
+}
