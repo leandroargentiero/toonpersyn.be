e07a94b6402e4753d4b3789771f8cd10e9e93a75
Leandro Argentiero <argentiero.leo@gmail.com>
2022-02-19 16:38:33 +0100

    Added IBMPlexMono google font as primary font

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello World!</h1>
+    <h1 className="p-3 flex flex-col font-primary text-slate-800">
+      <span>Toon Persyn</span>
+      <span className="">Director Of Photography</span>
+    </h1>
   );
 }
