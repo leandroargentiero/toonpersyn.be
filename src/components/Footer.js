d815e6b6befd58411682bb5561ef96e3b44fbd65
Leandro Argentiero <argentiero.leo@gmail.com>
2022-02-22 16:15:20 +0100

    Added primary font to footer elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="mt-24 flex flex-col justify-center pb-12 text-center">
+    <footer className="mt-24 flex flex-col justify-center pb-12 text-center font-primary">
       <p className="mb-2 text-xs">{`©Toon Persyn ${currentYear} - All Rights Reserved`}</p>
       <p className="text-xs text-gray-400">
         website by{' '}
