2544ec18df7a5d7690d9ee3c48e29132bc37ebda
Leandro Argentiero <argentiero.leo@gmail.com>
2022-02-23 10:45:06 +0100

    Added default props

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,12 +1,8 @@
 import ClipLoader from 'react-spinners/SquareLoader';
 
-const Loader = ({ bgColor, iconColor, size }) => {
+const Loader = ({ bgColor = 'bg-gray-50', iconColor = '#e5e7eb', size }) => {
   return (
-    <div
-      className={`relative z-10 h-full w-full ${
-        bgColor ? bgColor : 'bg-gray-100'
-      }`}
-    >
+    <div className={`relative z-10 h-full w-full ${bgColor}`}>
       <div className="absolute left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%]">
         <ClipLoader color={iconColor} size={size} />
       </div>
