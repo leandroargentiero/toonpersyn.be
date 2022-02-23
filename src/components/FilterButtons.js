9478ded1db5a8f42b6ac803187a2fc144faa1183
Leandro Argentiero <argentiero.leo@gmail.com>
2022-02-23 12:33:51 +0100

    Simplified UI

--- src/components/FilterButtons.js
+++ src/components/FilterButtons.js
@@ -7,10 +7,10 @@ const FilterButtons = ({ categories, handleFilterClick, filter }) => {
             <button
               key={category}
               value={category}
-              className={`rounded border-[1px] border-gray-100 py-1 px-3 font-primary text-xs text-gray-800 ${
+              className={`rounded p-2 p-3 font-primary text-xs text-gray-800 ${
                 category === filter
                   ? 'bg-gray-100 hover:bg-gray-200'
-                  : 'bg-white hover:bg-gray-100 active:bg-gray-200 '
+                  : 'bg-white hover:bg-gray-100 active:bg-gray-200'
               }`}
               onClick={handleFilterClick}
             >
