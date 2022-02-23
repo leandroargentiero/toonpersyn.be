ded172ab30acd0871eac675ed24f7adb4dd868ee
Leandro Argentiero <argentiero.leo@gmail.com>
2022-02-23 16:52:23 +0100

    Added analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,30 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+const GA_MEASUREMENT_ID = 'G-27LCX84SV1';
+
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_MEASUREMENT_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
