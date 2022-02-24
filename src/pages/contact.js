9b83a6a35bb628b5d40410f0b262381a6fe9c890
Leandro Argentiero <argentiero.leo@gmail.com>
2022-02-24 11:22:04 +0100

    Updated UI

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,7 +11,50 @@ export default function Contact() {
         <title>TOON PERSYN | CONTACT</title>
       </Head>
       <Container>
-        <div className="grid place-items-center justify-around py-16 text-center font-primary">
+        <div className="flex h-[60vh] flex-col justify-evenly font-primary text-sm">
+          <section className="content mt-6 grid grid-rows-3 justify-items-start gap-y-2 md:gap-y-0">
+            <h4 className="mb-2 font-bold uppercase">Details</h4>
+            <p>+32472271589</p>
+            <p>info@toonpersyn.be</p>
+            <p>BE 0659.774.994</p>
+          </section>
+          <section className="mt-6 grid grid-rows-3 justify-items-end gap-y-2">
+            <h4 className="mb-2 inline-block font-bold uppercase">socials</h4>
+            <a
+              href="https://www.imdb.com/name/nm8900949/"
+              className="underline hover:text-yellow-500"
+              target="_blank"
+              rel="noreferrer"
+            >
+              IMDb
+            </a>
+            <a
+              href="https://vimeo.com/toonpersyn"
+              className="underline hover:text-yellow-500"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Vimeo
+            </a>
+            <a
+              href="https://www.instagram.com/toonpersyn/"
+              className="underline hover:text-yellow-500"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
+            <a
+              href="https://www.linkedin.com/in/toon-persyn-7009b0149/"
+              className="underline hover:text-yellow-500"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+          </section>
+        </div>
+        {/* <div className="grid place-items-center justify-around py-16 text-center font-primary">
           <article className="shadow-gray-20  rounded-sm bg-white p-8 text-sm shadow-md">
             <section>
               <div className="relative mx-auto h-20 w-20 overflow-hidden rounded-full">
@@ -69,7 +112,7 @@ export default function Contact() {
               </a>
             </section>
           </article>
-        </div>
+        </div> */}
       </Container>
     </Layout>
   );
