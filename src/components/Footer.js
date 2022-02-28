291a7a9c43f42452d9ca55413fbe3642fff17070
Leandro Argentiero <argentiero.leo@gmail.com>
2022-02-28 10:33:17 +0100

    Removed mail link for MAREMOTO

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,7 @@ const Footer = () => {
   return (
     <footer className="mt-24 flex flex-col justify-center pb-12 text-center font-primary">
       <p className="mb-2 text-xs">{`©Toon Persyn ${currentYear} - All Rights Reserved`}</p>
-      <p className="text-xs text-gray-400">
-        website by{' '}
-        <Link href="mailto:leandro@maremoto.be">
-          <a className="hover:text-yellow-500">MAREMOTO</a>
-        </Link>
-      </p>
+      <p className="text-xs text-gray-400">website by MAREMOTO</p>
     </footer>
   );
 };
