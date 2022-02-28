1d44e77c9ab14cfcd6722e9a1ed617a8316bbce4
Leandro Argentiero <argentiero.leo@gmail.com>
2022-02-28 11:58:47 +0100

    Made layout changes to project detail images

--- src/components/ProjectBody.js
+++ src/components/ProjectBody.js
@@ -2,7 +2,7 @@ import { PrismicRichText } from '@prismicio/react';
 
 const ProjectBody = ({ content }) => {
   return (
-    <section className="container prose mr-auto max-w-3xl font-primary text-sm hover:prose-a:text-yellow-500 dark:text-neutral-200 dark:prose-headings:text-neutral-200 dark:prose-a:text-neutral-200 dark:prose-strong:text-neutral-200">
+    <section className="container prose mr-auto max-w-3xl font-primary text-sm hover:prose-a:text-yellow-500 dark:text-neutral-200 dark:prose-headings:text-neutral-200 dark:prose-a:text-neutral-200 dark:prose-strong:text-neutral-200 md:prose-img:w-[65%]">
       <PrismicRichText field={content} />
     </section>
   );
